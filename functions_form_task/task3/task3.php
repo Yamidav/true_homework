0cd688124a87b7df08289382d85405fd091c437b
Vadim <yamidav01@gmail.com>
2016-10-15 13:10:00 +0300

    Making gavno-code with preg_replace task3

--- functions_form_task/task3/task3.php
+++ functions_form_task/task3/task3.php
@@ -1,27 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <title> Task3 </title>
+</head>
+<body>
+<H1>Задание номер 3</H1>
+<form method="post">
+    <input type="number" name="lentgh">
+    <input type="submit" name="button" value="Отправить">
+</form>
 <?php
-function cbck($a)
-{
-    //$b=($_POST['lentgh']); тут будет ссылка на форму в html файле
-    $b = 5;
-    if (mb_strlen($a) >= $b) {
-        return 1;
+function rewrite($c) {
+    if (empty($_POST)){
+        die ("PLS TYPE NUMBER");
     } else {
-        return 0;
+        intval($_POST['lentgh']);
     }
-}
-$b = 5;
-$fp= fopen('text.txt', 'r');
-var_dump($fp);
-$text1=file_get_contents('./text.txt');
-var_dump($text1);
-//$text1="Текст какой, тЇо с кучїй символів, вот такая: фигня."; **для провери нижней части кода
-$text2=preg_replace('/[^a-zа-яіїіЇ\040]+/iu',"\040",$text1);
+$text1=file_get_contents("./$c");
+$text2=preg_replace('/[^\w\040]+/ui',"\040",$text1);
 $array1= explode(" ",$text2);
 $badWords = array_filter($array1,'cbck');
 foreach ($badWords as &$value){
     $value="/$value/";
 }
 $result=preg_replace ($badWords,"\040",$text1);
-echo "Отредактированный текст cо словами меньше {$b}: $result";
-unset ($value);
-unset ($b);
+echo "Отредактированный текст cо словами меньше {$_POST['lentgh']}: <br> $result";
+}
+function cbck($a){
+    $b=($_POST['lentgh']);
+    if (mb_strlen($a) >= $b) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+?>
+<?php rewrite ('text.txt')?>
+</body>
+</html>
